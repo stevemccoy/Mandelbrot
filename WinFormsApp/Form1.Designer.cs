a41152a98a4989cbc09189c923ab29a59a2914d8
SteveMcCoy <steve_mccoy@hotmail.co.uk>
2018-04-02 15:22:40 +0100

    Add the first code

---
+++ WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 400);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 0);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private const int NumColors = 256;
+        private const int MaxCount = 10000;
+
+        private readonly Random _myRandom = new Random(DateTime.UtcNow.Millisecond * 1000 + DateTime.UtcNow.Second);
+        private Color[] _colors;
+        Bitmap _myBitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupColors();
+        }
+
+        public void SetMessage(string message) => Text = message;
+
+        private void SetupColors()
+        {
+            var colors = new Color[NumColors];
+            colors[0] = Color.FromArgb(0, 0, 0);
+            colors[MaxCount % NumColors] = Color.FromArgb(0, 0, 0);
+            for (var i = 1; i < NumColors; i++)
+            {
+                var r = _myRandom.Next() % 256;
+                var g = _myRandom.Next() % 256;
+                var b = _myRandom.Next() % 256;
+                colors[i] = Color.FromArgb(r, g, b);
+            }
+            _colors = colors;
+        }
+
+        private void SetupImage()
+        {
+            _myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, PixelFormat.Format24bppRgb);
+            ColourImage();
+            pictureBox1.Image = _myBitmap;
+        }
+
+        private void ColourImage()
+        {
+            Color c1 = Color.Green;
+            for (int gx = 0; gx < _myBitmap.Width; gx++)
+            {
+                for (int gy = 0; gy < _myBitmap.Height; gy++)
+                {
+                    _myBitmap.SetPixel(gx, gy, c1);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetupImage();
+        }
+    }
+}
